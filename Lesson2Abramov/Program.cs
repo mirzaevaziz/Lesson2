b2a4a021f904a8197a23a559c2873ac50c704d72
Aziz Mirzaev <mirzaevaziz@gmail.com>
2022-10-04 17:15:34 +0500

    init 2

---
+++ Lesson2Abramov/Program.cs
@@ -0,0 +1,12 @@
+﻿{
+    decimal a, b;
+    System.Console.Write("a = ");
+    a = Convert.ToDecimal(Console.ReadLine());
+    System.Console.Write("b = ");
+    b = Convert.ToDecimal(Console.ReadLine());
+
+    System.Console.WriteLine("a + b = {0}", a + b);
+    System.Console.WriteLine("a - b = {0}", a - b);
+    System.Console.WriteLine("a * b = {0}", a * b);
+    System.Console.WriteLine($@"a * b = {a * b}");
+}
---
+++ Lesson2Sample1/Program.cs
@@ -0,0 +1,5 @@
+﻿//Imya so'rash
+string imya = Console.ReadLine();
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Привет, {imya}!");
